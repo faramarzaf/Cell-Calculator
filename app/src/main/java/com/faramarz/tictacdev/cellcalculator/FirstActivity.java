4d1aade8c71c6d75efb2c6db1b1a725f2970dcce
FaramarzAf <faramarzafzali@gmail.com>
2019-05-15 20:18:01 +0430

    first screen added

---
+++ app/src/main/java/com/faramarz/tictacdev/cellcalculator/FirstActivity.java
@@ -0,0 +1,40 @@
+package com.faramarz.tictacdev.cellcalculator;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FirstActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn_ic50, btn_anova,btn_doublingTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+        bind();
+
+        btn_ic50.setOnClickListener(this);
+        btn_anova.setOnClickListener(this);
+        btn_doublingTime.setOnClickListener(this);
+    }
+
+    void bind(){
+        btn_ic50 = findViewById(R.id.btn_ic50);
+        btn_anova = findViewById(R.id.btn_anova);
+        btn_doublingTime = findViewById(R.id.btn_doublingTime);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId()==btn_ic50.getId()){
+
+        }else if (v.getId()==btn_anova.getId()){
+
+        }else if(v.getId()==btn_doublingTime.getId()){
+            startActivity(new Intent(this,MainActivity.class));
+        }
+    }
+}
